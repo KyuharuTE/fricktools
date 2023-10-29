55b6a1f26a52f999b275bbc210bf227602d2def2
Kyuharu <kyuharu@qq.com>
2023-10-29 12:03:44 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -5,6 +5,6 @@ from menu import menu
 def main():
     input('''免责声明\n---------\n1. 本工具仅起到辅助作用，因本工具造成的财产等损失，本工具作者概不负责！\n2. 在使用特定功能拥有特定的免责声明，如不能同意因停止使用该功能!\n3. 本工具由 Kyuharu 使用Python3编写，部分源码收集于互联网，如有侵权请联系邮箱tyuharu@foxmail.com\n---------\n按Enter继续...''')
 
-    menu.home()
+    menu.home(self)
 
 main()
